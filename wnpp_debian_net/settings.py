b8458c6f28bc412655f063f01527aa607c5bc357
Sebastian Pipping <sebastian@pipping.org>
2021-02-14 19:58:12 +0100

    settings: Default DEBUG to False

--- wnpp_debian_net/settings.py
+++ wnpp_debian_net/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_o_p1732gublv5gf9nk6_k!m3kvp+vv)b_j*g+h*n9gr6mc@71'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('WDN_DEBUG') == 'true'
 
 ALLOWED_HOSTS = (os.environ.get('WDN_ALLOWED_HOSTS') or ','.join([
     '0.0.0.0',
