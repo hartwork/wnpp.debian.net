6f0b115f378441406abf02a0bc1cd5c9ec526670
Sebastian Pipping <sebastian@pipping.org>
2021-02-15 15:43:46 +0100

    coverage: Integrate

--- wnpp_debian_net/settings.py
+++ wnpp_debian_net/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'debug': 'test' in sys.argv,  # for django_coverage_plugin
         },
     },
 ]
