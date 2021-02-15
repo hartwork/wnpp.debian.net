0f79b2ab82f3d628983baca02c5bc54a872d5513
Sebastian Pipping <sebastian@pipping.org>
2021-02-15 04:12:57 +0100

    contact_links.py: Cover contact_link_for

--- wnpp_debian_net/templatetags/tests/test_contact_links.py
+++ wnpp_debian_net/templatetags/tests/test_contact_links.py
@@ -6,6 +6,7 @@ from unittest import TestCase
 from parameterized import parameterized
 
 from ...templatetags.contact_links import _parse_contact
+from ..contact_links import contact_link_for
 
 
 class ParseContactTest(TestCase):
@@ -18,3 +19,17 @@ class ParseContactTest(TestCase):
         actual_mailto, actual_display = _parse_contact(contact)
         self.assertEqual(actual_mailto, 'mail@example.org')
         self.assertEqual(actual_display, expected_display)
+
+
+class ContactLinkForTest(TestCase):
+    @parameterized.expand([
+        (None, '<i>nobody</i>'),
+        ('', '<i>nobody</i>'),
+        ('no at sign in sight', '<i>no at sign in sight</i>'),
+        ('Some Thing <wellformed@example.org>',
+         '<a href="mailto:wellformed@example.org">Some Thing</a>'),
+        ('just@example.org', '<a href="mailto:just@example.org">just@example.org</a>'),
+    ])
+    def test_contact_link_for(self, contact, expected_html):
+        actual_html = contact_link_for(contact)
+        self.assertEqual(actual_html, expected_html)
