c8568320f8737f11f666dcef0ed258b11340ff7a
Sebastian Pipping <sebastian@pipping.org>
2021-02-19 18:46:07 +0100

    pagination.py: Cover template

--- wnpp_debian_net/tests/test_pagination.py
+++ wnpp_debian_net/tests/test_pagination.py
@@ -3,6 +3,10 @@
 
 from unittest import TestCase
 
+from bs4 import BeautifulSoup
+from django.core.paginator import Paginator
+from django.template.loader import get_template
+from django.test import RequestFactory
 from parameterized import parameterized
 
 from ..pagination import ELLIPSIS, iterate_page_items
@@ -53,3 +57,90 @@ class PageItemsTest(TestCase):
         actual_page_items = list(iterate_page_items(total_page_count, current_page_number))
         self.assertEqual(actual_page_items,
                          [1, 2, ELLIPSIS, 220, 221, 222, 223, 224, ELLIPSIS, 554, 555])
+
+
+class PaginationTemplateTest(TestCase):
+    ending_item_count = 2  # bigger than 1 and as small as possible
+    max_item_count = 9  # minimum given ``ending_item_count = 2``
+    items_per_page = 2  # bigger than one, as small as possible
+    page_count = max_item_count + 2  # allow for up to two ellipses and as small as possible
+    url = '/'  # arbitrary
+
+    def setUp(self) -> None:
+        self.paginator = Paginator(object_list=range(self.page_count * self.items_per_page),
+                                   per_page=self.items_per_page)
+        self.template = get_template('pagination.html')
+
+    @staticmethod
+    def _extract_page_page_item_classes(soup):
+        def without_class_page_item(it):
+            return [clazz for clazz in it if clazz != 'page-item']
+
+        return [without_class_page_item(li_tag['class']) for li_tag in soup.find_all('li')]
+
+    @staticmethod
+    def _extract_link_text(soup):
+        return [a_tag.string for a_tag in soup.find_all('a')]
+
+    @parameterized.expand([
+        (1, [
+            ('Previous', ['disabled']),
+            ('1', ['active']),
+            ('2', []),
+            ('3', []),
+            ('4', []),
+            ('5', []),
+            ('6', []),
+            ('…', ['disabled']),
+            ('10', []),
+            ('11', []),
+            ('Next', []),
+        ], '(1 to 2; 22 total)'),
+        (6, [
+            ('Previous', []),
+            ('1', []),
+            ('2', []),
+            ('…', ['disabled']),
+            ('5', []),
+            ('6', ['active']),
+            ('7', []),
+            ('…', ['disabled']),
+            ('10', []),
+            ('11', []),
+            ('Next', []),
+        ], '(11 to 12; 22 total)'),
+        (11, [
+            ('Previous', []),
+            ('1', []),
+            ('2', []),
+            ('…', ['disabled']),
+            ('6', []),
+            ('7', []),
+            ('8', []),
+            ('9', []),
+            ('10', []),
+            ('11', ['active']),
+            ('Next', ['disabled']),
+        ], '(21 to 22; 22 total)'),
+    ])
+    def test(self, current_page, expected_display, expected_summary):
+        page_items = iterate_page_items(total_page_count=self.page_count,
+                                        current_page_number=current_page,
+                                        max_item_count=self.max_item_count,
+                                        ending_item_count=self.ending_item_count)
+        data = {
+            'page': current_page,
+        }
+        context = {
+            'page_items': page_items,
+            'page_obj': self.paginator.get_page(current_page),
+            'request': RequestFactory().get(self.url, data),
+        }
+
+        actual_content = self.template.render(context)
+
+        soup = BeautifulSoup(markup=actual_content, features='html.parser')
+        actual_display = list(
+            zip(self._extract_link_text(soup), self._extract_page_page_item_classes(soup)))
+        self.assertEqual(actual_display, expected_display)
+        self.assertIn(expected_summary, actual_content)
